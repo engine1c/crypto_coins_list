241e436135e9396a12c3d1c30982e758840c070f
engine1c <111049622+engine1c@users.noreply.github.com>
2023-05-08 16:34:31 +0200

    #15 - AutoRoute -ok

--- lib/crypto_coins_list_app.dart
+++ lib/crypto_coins_list_app.dart
@@ -4,21 +4,30 @@ import 'package:crypto_coins_list/theme/theme.dart';
 import 'package:get_it/get_it.dart';
 import 'package:talker_flutter/talker_flutter.dart';
 
-class CryptoApp extends StatelessWidget {
+class CryptoApp extends StatefulWidget {
   const CryptoApp({super.key});
 
+  @override
+  State<CryptoApp> createState() => _CryptoAppState();
+}
+
+class _CryptoAppState extends State<CryptoApp> {
+  final _appRouter = AppRouter();    
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
+    return MaterialApp.router(
       
       title: '___crypto___',
       theme: darkTheme,
-      routes: routes,
-      initialRoute: '/main',
-      navigatorObservers: [
-        TalkerRouteObserver(GetIt.I<Talker>()),
-      ],
-       );
+      routerConfig: _appRouter.config(
+       navigatorObservers: () => [
+        TalkerRouteObserver(GetIt.I<Talker>() 
+          ),
+        ],
+      //routes: routes,
+      //initialRoute: '/main',
+      ),
+    );
   }
 }
--- lib/router/router.dart
+++ lib/router/router.dart
@@ -1,7 +1,25 @@
+import 'package:auto_route/auto_route.dart';
 import 'package:crypto_coins_list/features/crypto_coin/crypto_coin.dart';
 import 'package:crypto_coins_list/features/crypto_list/crypto_list.dart';
+import 'package:crypto_coins_list/repositories/crypto_coins/crypto_coins.dart';
+import 'package:flutter/material.dart';
+part 'router.gr.dart';  
 
-final routes = {
-        '/main' : (context) => const CryptoListScreen(),
-      '/coin' : (context) => const CryptoCoinScreen(),
-      };
+// final routes = {
+//         '/main' : (context) => const CryptoListScreen(),
+//       '/coin' : (context) => const CryptoCoinScreen(),
+//       };
+
+
+          
+ //flutter packages pub run build_runner build / watch            
+@AutoRouterConfig()      
+class AppRouter extends _$AppRouter {      
+    
+  @override      
+  List<AutoRoute> get routes => [ 
+    AutoRoute(page: CryptoListRoute.page, path: '/'),
+    AutoRoute(page: CryptoCoinRoute.page),     
+   /// routes go here     
+   ];    
+ }        
